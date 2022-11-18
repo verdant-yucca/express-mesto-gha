24fa5792b4eeb5b678ae2c45c151f85398cb6917
Roman Davydenko <Deadshot323rd@yandex.ru>
2022-11-19 00:47:28 +0800

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -15,10 +15,10 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
   }
 
-  // if (req.method === 'OPTIONS') {
-  //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  //   res.header('Access-Control-Allow-Headers', requestHeaders);
-  //   return res.end();
-  // }
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   return next();
 };
