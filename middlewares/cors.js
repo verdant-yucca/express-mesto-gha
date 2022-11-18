416b33be4f06694013cfc6d4a9542d086c270705
Roman Davydenko <Deadshot323rd@yandex.ru>
2022-11-19 00:13:41 +0800

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,24 +5,19 @@ const allowedCors = [
 ];
 
 const cors = (req, res, next) => {
-  console.log('сюда попал');
   const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    console.log('сюда попал1');
-
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
-    console.log('сюда попал2');
-
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    return res.end();
   }
   return next()
 }
