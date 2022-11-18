fee1c06e8a691a7bcc151ad5b831eca4a45dcdcf
Roman Davydenko <Deadshot323rd@yandex.ru>
2022-11-19 03:01:22 +0800

    fix cors

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
+const cors = require('cors');
 const centralizedError = require('./middlewares/centralizedError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/routes');
@@ -11,11 +12,16 @@ const router = require('./routes/routes');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const corsOptions = {
+  origin: 'https://verdant-yucca-front.nomoredomains.icu',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb'); // localhost || 127.0.0.1
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.use(cors);
+app.use(cors(corsOptions));
 
 app.use(router);
 
