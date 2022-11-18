84302913b6211ebfec99a111c90ddd5df550e329
Roman Davydenko <Deadshot323rd@yandex.ru>
2022-11-19 01:44:28 +0800

    fix cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const centralizedError = require('./middlewares/centralizedError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/routes');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb'); // localhost || 127.0.0.1
