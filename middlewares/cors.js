227de30d3fae4c7d10de974a3fe0f8b687188a3f
Roman Davydenko <Deadshot323rd@yandex.ru>
2022-11-18 23:43:09 +0800

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
-  'https://verdant-yucca-front.nomoredomains.icu',
-  'http://verdant-yucca-front.nomoredomains.icu',
+  'https://verdant-yucca-front.nomoredomains.icu/',
+  'http://verdant-yucca-front.nomoredomains.icu/',
   'localhost:3000'
 ];
 
