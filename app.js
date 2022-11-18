4d1827efc2cd2973595be9d8954c5f6ab3f72bd5
Roman Davydenko <Deadshot323rd@yandex.ru>
2022-11-19 01:46:45 +0800

    fix cors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -8,14 +8,14 @@ const centralizedError = require('./middlewares/centralizedError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/routes');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb'); // localhost || 127.0.0.1
-
+console.log('22');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(requestLogger);
+// app.use(requestLogger);
 app.use(cors);
 
 app.use(router);
